366982fa64e9026540abec1579e457b81653ea4d
Bernie FitzGerald <bernardf@live.com.au>
2023-03-07 14:16:27 +1100

    Lorem Ipsum text added. More complicated version of Feedback component (without JSX)

--- Feedback.js
+++ Feedback.js
@@ -1,17 +1,72 @@
 function Feedback() {
-	return React.createElement(
-		'button',
-		{
-		  onClick: () => showTextBox(),
-		},
-		'Click me',
-	  );
+  const [buttonText, setButtonText] = React.useState("Give Feedback");
+  const [buttonColor, setButtonColor] = React.useState("darkGreen");
+  const [textAreaText, setTextAreaText] = React.useState("");
+  const [showTextArea, setShowTextArea] = React.useState(false);
+
+  return React.createElement(
+    "div",
+    {
+      style: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        width: "450px",
+        position: "fixed",
+        right: "15px",
+        bottom: "15px",
+        zIndex: "1000",
+        alignItems: "flex-end",
+      },
+    },
+    React.createElement("textarea", {
+      placeholder: "Type your feedback comments here...",
+      style: {
+        display: showTextArea ? "block" : "none",
+        height: "150px",
+        padding: "12px 20px",
+        boxSizing: "border-box",
+        border: "2px solid #ccc",
+        borderRadius: "12px",
+        backgroundColor: "#f8f8f8",
+        fontSize: "16px",
+        resize: "none",
+        width: "100%",
+      },
+      value: textAreaText,
+      onChange: (e) => setTextAreaText(e.target.value),
+    }),
+    React.createElement(
+      "button",
+      {
+        style: {
+          border: "3px solid " + buttonColor,
+          borderRadius: "32px",
+          backgroundColor: buttonColor,
+          fontSize: "20px",
+          color: "white",
+          width: "150px",
+          padding: "15px",
+        },
+        onClick: () => {
+          if (buttonColor === "darkGreen") {
+            setButtonText("Submit Feedback");
+            setButtonColor("orange");
+            setShowTextArea(true);
+          } else {
+            // Make ajax post request to send the feedback to some endpoint
+            setShowTextArea(false);
+            setTextAreaText("");
+            setButtonText("Submitted");
+            setButtonColor("blue");
+          }
+        },
+      },
+      buttonText
+    )
+  );
 }
 
-function showTextBox() {
-	alert('Hello Bernie')
-}
-
-const rootNode = document.getElementById('react-entry-point');
+const rootNode = document.getElementById("react-entry-point");
 const root = ReactDOM.createRoot(rootNode);
 root.render(React.createElement(Feedback));
